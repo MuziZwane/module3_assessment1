073b2bed9c5b140cb851b54d9f68a257465bdce5
Muzi Zwane <87149575+MuziZwane@users.noreply.github.com>
2022-09-08 15:22:56 +0200

    Update AboutUs.js

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
+export default class App extends Component {
+     render() {
+       return (
+         
+         
+       )
+     }
+
+
+}
