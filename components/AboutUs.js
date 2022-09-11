28245cca0f9bf961e08cf405434742d2471d341b
Muzi Zwane <87149575+MuziZwane@users.noreply.github.com>
2022-09-11 10:16:39 +0200

    changed the splashscreen and dashboard

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-export default class App extends Component {
-     render() {
-       return (
-         
-         
-       )
-     }
+export default function AboutUs() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Welcome to the About-Us page.
 
-
+        We are aspiring software developers, focused mainly on mobile app development.
+      </Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
